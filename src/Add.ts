e88e81a5717d1e4665bce2af159166d10fcb3526
Priyanka Khatarkar <khatarkar.priyanka456@gmail.com>
2024-11-20 18:25:11 +0530

    Refactor code and added comments

--- src/Add.ts
+++ src/Add.ts
@@ -1,14 +1,21 @@
-export default function Add(
-  numberString: string,
-  delimeter: string = ","
-): number | void {
-  let numberArray = numberString.split(delimeter);
-  let negativeNumberArray = numberArray.filter((val) => Number(val) < 0);
-  if (negativeNumberArray.length > 0) {
-    throw new Error("negative numbers not allowed");
+/**
+ * @description Function to add a string of numbers. negative numbers are not allowed.
+ * @param numbers string
+ * @param delimiter string
+ * @returns 
+ */
+export default function add(numbers: string, delimiter: string = ","): number {
+    // Split the input string into an array of numbers
+    const parsedNumbers = numbers.split(delimiter).map(Number);
+  
+    // Find negative numbers
+    const negativeNumbers = parsedNumbers.filter(num => num < 0);
+  
+    // Throw an error if any negative numbers are found
+    if (negativeNumbers.length > 0) {
+        throw new Error("negative numbers not allowed");
+    }
+  
+    // Sum up all the numbers
+    return parsedNumbers.reduce((sum, num) => sum + num, 0);
   }
-  let output = numberArray.reduce((total, val) => {
-    return total + Number(val);
-  }, 0);
-  return output;
-}
