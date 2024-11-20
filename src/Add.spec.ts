eb7b5a7a468fb8ae64934f2c3e101c65b6ac8b6b
Priyanka Khatarkar <khatarkar.priyanka456@gmail.com>
2024-11-20 17:59:56 +0530

    Added more unit test case as per sample examples

--- src/Add.spec.ts
+++ src/Add.spec.ts
@@ -3,6 +3,14 @@ import add from "./Add";
 
 describe("first", () => {
   it("expect addition of two numbers to be correct", () => {
-    expect(add(2,2)).toBe(4);
+    expect(add("2,2")).toBe(4);
+  });
+
+  it("expect empty string output to be zero", () => {
+    expect(add("")).toBe(0);
+  });
+
+  it("expect on passing single value in string the output will be same value", () => {
+    expect(add("2")).toBe(2);
   });
 });
